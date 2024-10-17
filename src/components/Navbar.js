5da97c158e9303a19ee1d935aac1ffe021176497
Adam G <adam.gnatowski@performance-media.pl>
2024-10-17 15:32:29 +0200

    nabar mobile menu collapse added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ import "./Navbar.css";
 
 function Navbar() {
   const [user, setUser] = useState(null);
+  const [isOpen, setIsOpen] = useState(false); // State to manage navbar collapse
 
   useEffect(() => {
     // Set up an authentication state observer
@@ -19,6 +20,13 @@ function Navbar() {
     return () => unsubscribe();
   }, []);
 
+  // Function to handle link clicks
+  const handleLinkClick = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -28,49 +36,69 @@ function Navbar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          onClick={() => setIsOpen(!isOpen)} // Toggle the isOpen state
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={isOpen}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        {/* Conditionally add 'show' class based on isOpen state */}
+        <div
+          className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={handleLinkClick}>
                 Home
               </Link>
             </li>
             {user && (
               <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">
+                <Link
+                  className="nav-link"
+                  to="/dashboard"
+                  onClick={handleLinkClick}
+                >
                   Dashboard
                 </Link>
               </li>
             )}
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link
+                className="nav-link"
+                to="/contact"
+                onClick={handleLinkClick}
+              >
                 Contact Us
               </Link>
             </li>
             {!user ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">
+                  <Link
+                    className="nav-link"
+                    to="/login"
+                    onClick={handleLinkClick}
+                  >
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/register">
+                  <Link
+                    className="nav-link"
+                    to="/register"
+                    onClick={handleLinkClick}
+                  >
                     Register
                   </Link>
                 </li>
               </>
             ) : (
               <li className="nav-item">
-                <Logout />
+                <Logout onClick={handleLinkClick} />{" "}
+                {/* Ensure Logout also collapses the menu */}
               </li>
             )}
           </ul>
